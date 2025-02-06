0e8ed02290b07530e30ce0205df8936bd22b4d04
jouissance seeker <051.sys113@gmail.com>
2025-02-06 21:07:57 +0330

    refactor: before refactor cart and favorites

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,14 +1,13 @@
-import { ProductCard } from '@/components/product-card';
-import { categoriesData } from '@/resources/categories';
+import {
+  ArchiveList,
+  FilterCategories,
+} from '@/containers/routes/explore/archive-list';
 
 export default function Page() {
   return (
-    <div className="container grid h-fit grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
-      {categoriesData
-        .flatMap((category) => category.children)
-        .map((item, index) => (
-          <ProductCard key={index} data={item} />
-        ))}
+    <div className="container grid h-full grid-cols-[250px_1fr]">
+      <FilterCategories />
+      <ArchiveList />
     </div>
   );
 }

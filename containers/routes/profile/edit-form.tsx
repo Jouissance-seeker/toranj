bcfff782b716acbd4ad1e77cba04c2290e32ddc2
jouissance seeker <051.sys113@gmail.com>
2025-02-10 11:59:06 +0330

    refactor: refactor forms

--- containers/routes/profile/edit-form.tsx
+++ containers/routes/profile/edit-form.tsx
@@ -3,30 +3,60 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { useApiCall } from '@/hooks/api-call';
-import { APIlogin } from '@/services/login';
 import { cn } from '@/utils/cn';
 
 export function EditForm() {
-  const [callApiSubmitBtn, isLoadingSubmitBtn] = useApiCall();
+  const formFields = {
+    firstName: {
+      label: 'نام',
+      type: 'text',
+      errors: {
+        least3characters: 'نام باید حداقل 3 کاراکتر باشد!',
+      },
+    },
+    lastName: {
+      label: 'نام خانوادگی',
+      type: 'text',
+      errors: {
+        least3characters: 'نام خانوادگی باید حداقل 3 کاراکتر باشد!',
+      },
+    },
+    email: {
+      label: 'ایمیل',
+      type: 'email',
+      errors: {
+        isNotCorrect: 'ایمیل وارد شده معتبر نیست!',
+      },
+    },
+    phoneNumber: {
+      label: 'شماره موبایل',
+      type: 'number',
+      errors: {
+        isNotCorrect: 'شماره موبایل وارد شده معتبر نیست!',
+      },
+    },
+    address: {
+      label: 'آدرس',
+      type: 'text',
+      errors: {
+        least8characters: 'آدرس باید حداقل 8 کاراکتر باشد!',
+      },
+    },
+  };
   const formSchema = z.object({
     firstName: z.string().min(3, {
-      message: 'نام باید حداقل 3 کاراکتر باشد!',
+      message: formFields.firstName.errors.least3characters,
     }),
     lastName: z.string().min(3, {
-      message: 'نام خانوادگی باید حداقل 3 کاراکتر باشد!',
+      message: formFields.lastName.errors.least3characters,
     }),
-    email: z.string().email('ایمیل وارد شده معتبر نیست!'),
+    email: z.string().email(formFields.email.errors.isNotCorrect),
     phoneNumber: z.string().regex(new RegExp(/^0\d{10}$/), {
-      message: 'شماره موبایل وارد شده معتبر نیست!',
+      message: formFields.phoneNumber.errors.isNotCorrect,
     }),
-    address: z
-      .string({
-        required_error: 'آدرس الزامی است',
-      })
-      .min(8, {
-        message: 'آدرس باید حداقل 8 کاراکتر باشد!',
-      }),
+    address: z.string().min(8, {
+      message: formFields.address.errors.least8characters,
+    }),
   });
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -38,18 +68,8 @@ export function EditForm() {
       address: '',
     },
   });
-  const handleSubmitForm = async (values: any) => {
-    callApiSubmitBtn(
-      APIlogin({
-        body: {
-          phone: values.phoneNumber,
-          password: values.password,
-        },
-      }),
-      () => {
-        // ...
-      },
-    );
+  const handleSubmitForm = async () => {
+    // ...
   };
 
   return (
@@ -59,103 +79,33 @@ export function EditForm() {
         className="flex w-[350px] flex-col rounded-xl border bg-white p-3 sm:w-[500px]"
       >
         {/* fields */}
-        <div className="mb-4 grid grid-cols-2 gap-2">
-          {/* first name */}
-          <div className="col-span-1">
-            <label htmlFor="firstName">نام</label>
-            <input
-              id="firstName"
-              {...form.register('firstName')}
-              className={cn({
-                'border-red-500': form.formState.errors.firstName,
-              })}
-            />
-            <p
-              className={cn({
-                hidden: !form.formState.errors.firstName?.message,
-              })}
-            >
-              {form.formState.errors.firstName?.message}
-            </p>
-          </div>
-          {/* last name */}
-          <div className="col-span-1">
-            <label htmlFor="lastName">نام خانوادگی</label>
-            <input
-              id="lastName"
-              {...form.register('lastName')}
-              className={cn({
-                'border-red-500': form.formState.errors.lastName,
-              })}
-            />
-            <p
-              className={cn({
-                hidden: !form.formState.errors.lastName?.message,
-              })}
-            >
-              {form.formState.errors.lastName?.message}
-            </p>
-          </div>
-          {/* email */}
-          <div className="col-span-1">
-            <label htmlFor="email">ایمیل</label>
-            <input
-              id="email"
-              {...form.register('email')}
-              className={cn({
-                'border-red-500': form.formState.errors.email,
-              })}
-            />
-            <p
-              className={cn({
-                hidden: !form.formState.errors.email?.message,
-              })}
-            >
-              {form.formState.errors.email?.message}
-            </p>
-          </div>
-          {/* phone */}
-          <div className="col-span-1">
-            <label htmlFor="phone">تلفن همراه</label>
-            <input
-              id="phone"
-              {...form.register('phoneNumber')}
-              type="number"
-              className={cn({
-                'border-red-500': form.formState.errors.phoneNumber,
-              })}
-            />
-            <p
-              className={cn({
-                hidden: !form.formState.errors.phoneNumber?.message,
-              })}
-            >
-              {form.formState.errors.phoneNumber?.message}
-            </p>
-          </div>
-          {/* address */}
-          <div className="col-span-2">
-            <label htmlFor="address">آدرس</label>
-            <textarea
-              id="address"
-              {...form.register('address')}
-              className={cn({
-                'border-red-500': form.formState.errors.address,
-              })}
-            />
-            <p
-              className={cn({
-                hidden: !form.formState.errors.address?.message,
-              })}
-            >
-              {form.formState.errors.address?.message}
-            </p>
-          </div>
+        <div className="mb-4 mt-2 grid gap-2">
+          {Object.entries(formFields).map(([key, field]) => {
+            const error =
+              form.formState.errors[
+                key as keyof z.infer<typeof formSchema>
+              ]?.message?.toString();
+            return (
+              <div key={key} className="flex flex-col">
+                <label htmlFor={key} className="mb-1 text-sm text-teal">
+                  {field.label}
+                </label>
+                <input
+                  id={key}
+                  type={field.type}
+                  {...form.register(key as keyof z.infer<typeof formSchema>)}
+                  className={cn({ 'border-red-500': !!error })}
+                />
+                {error ? (
+                  <p className="mt-1 text-xs text-red-500">{error}</p>
+                ) : null}
+              </div>
+            );
+          })}
         </div>
         {/* submit */}
         <button
           type="submit"
-          disabled={isLoadingSubmitBtn}
           className="rounded-lg bg-teal p-4 text-white disabled:bg-teal/50"
         >
           ذخیره

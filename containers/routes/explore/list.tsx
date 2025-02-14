60f3c97fe2fa08aa887e3c573c14bd813e9f7078
jouissance seeker <051.sys113@gmail.com>
2025-02-14 11:51:54 +0330

    chore: get products in /explore

--- containers/routes/explore/list.tsx
+++ containers/routes/explore/list.tsx
@@ -1,14 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import { APIgetProducts } from '@/actions/routes/explore/get-products';
+import { Loader } from '@/components/loader';
 import { ProductCard } from '@/components/product-card';
-import { categoriesData } from '@/resources/categories';
 
 export function List() {
+  const fetchProducts = useQuery({
+    queryKey: ['products'],
+    queryFn: () => APIgetProducts(),
+  });
+
+  if (fetchProducts.isLoading) {
+    return <Loader />;
+  }
+
   return (
     <section className="col-span-1 grid h-fit grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2">
-      {categoriesData
-        .flatMap((category) => category.children)
-        .map((item, index) => (
-          <ProductCard key={index} data={item} />
-        ))}
+      {fetchProducts.data?.map((item) => (
+        <ProductCard key={item._id} data={item} />
+      ))}
     </section>
   );
 }

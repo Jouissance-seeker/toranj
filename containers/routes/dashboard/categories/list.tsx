419f81622c61d1a4d9f861f71d06d1fca169af10
jouissance seeker <051.sys113@gmail.com>
2025-03-03 16:44:58 +0330

    chore: new changes

--- containers/routes/dashboard/categories/list.tsx
+++ containers/routes/dashboard/categories/list.tsx
@@ -4,7 +4,6 @@ import { useQuery } from '@tanstack/react-query';
 import Image from 'next/image';
 import toast from 'react-hot-toast';
 import { IoMdTrash } from 'react-icons/io';
-import { IoBagAdd } from 'react-icons/io5';
 import { MdEdit } from 'react-icons/md';
 import { APIdeleteCategory } from '@/actions/routes/dashboard/categories/delete-category';
 import { APIgetCategories } from '@/actions/routes/global/get-categories';
@@ -47,77 +46,83 @@ export function List() {
     return <Loader />;
   }
 
-  if (fetchCategories.data?.length === 0) {
-    return <Empty text="دسته بندی برای نمایش وجود ندارد!" />;
-  }
-
   return (
-    <section className="size-full">
+    <section className="flex size-full flex-col gap-3">
+      {/* actions */}
+      <button
+        onClick={handleShowModalAddCategory}
+        className="w-fit rounded-lg bg-teal p-2 text-smp text-white"
+      >
+        افزودن دسته بندی
+      </button>
+      {/* table */}
       <div className="relative flex size-full h-fit flex-col overflow-auto rounded-xl border border-gray-200 bg-white bg-clip-border text-gray-600">
-        <table className="w-full min-w-max table-auto text-right text-sm">
-          <thead className="bg-gray-200">
-            <tr>
-              <th className="border-b border-gray-200 p-3">
-                <p className="block text-sm font-medium leading-none antialiased">
-                  #
-                </p>
-              </th>
-              <th className="border-b border-gray-200 p-3">
-                <p className="block text-sm font-medium leading-none antialiased">
-                  تصویر
-                </p>
-              </th>
-              <th className="border-b border-gray-200 p-3">
-                <p className="block text-sm font-medium leading-none antialiased">
-                  عنوان
-                </p>
-              </th>
-              <th className="border-b border-gray-200 p-3">
-                <button onClick={handleShowModalAddCategory}>
-                  <IoBagAdd size={22} />
-                </button>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {fetchCategories.data?.map((item, index) => (
-              <tr key={item._id} className="even:bg-gray-50">
-                <td className="px-4 py-1">{index + 1}</td>
-                <td className="px-4 py-1 text-center">
-                  <Image
-                    src={`${process.env.BASE_URL}${item.image.path}`}
-                    alt={item.title}
-                    width={50}
-                    height={50}
-                  />
-                </td>
-                <td className="max-w-[150px] truncate px-4 py-1 text-right">
-                  {item.title}
-                </td>
-                <td className="px-4 py-1 text-right">
-                  <button
-                    onClick={() =>
-                      handleShowModalEditCategory({
-                        title: item.title,
-                        image: item.image,
-                        id: item._id,
-                      })
-                    }
-                    className="mx-2"
-                  >
-                    <MdEdit size={22} />
-                  </button>
-                  <button
-                    onClick={() => handleDeleteCategory(item._id)}
-                    className="mx-2"
-                  >
-                    <IoMdTrash size={22} />
-                  </button>
-                </td>
+        {fetchCategories.data?.length !== 0 ? (
+          <table className="w-full min-w-max table-auto text-right text-sm">
+            <thead className="bg-gray-200">
+              <tr>
+                <th className="border-b border-gray-200 p-3">
+                  <p className="block text-sm font-medium leading-none antialiased">
+                    #
+                  </p>
+                </th>
+                <th className="border-b border-gray-200 p-3">
+                  <p className="block text-sm font-medium leading-none antialiased">
+                    تصویر
+                  </p>
+                </th>
+                <th className="border-b border-gray-200 p-3">
+                  <p className="block text-sm font-medium leading-none antialiased">
+                    عنوان
+                  </p>
+                </th>
+                <th />
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {fetchCategories.data?.map((item, index) => (
+                <tr key={item._id} className="even:bg-gray-50">
+                  <td className="px-4 py-1">{index + 1}</td>
+                  <td className="px-4 py-1 text-center">
+                    <Image
+                      src={`${process.env.BASE_URL}${item.image.path}`}
+                      alt={item.title}
+                      width={50}
+                      height={50}
+                    />
+                  </td>
+                  <td className="max-w-[150px] truncate px-4 py-1 text-right">
+                    {item.title}
+                  </td>
+                  <td className="px-4 py-1 text-right">
+                    <button
+                      onClick={() =>
+                        handleShowModalEditCategory({
+                          title: item.title,
+                          image: item.image,
+                          id: item._id,
+                        })
+                      }
+                      className="mx-2"
+                    >
+                      <MdEdit size={22} />
+                    </button>
+                    <button
+                      onClick={() => handleDeleteCategory(item._id)}
+                      className="mx-2"
+                    >
+                      <IoMdTrash size={22} />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <div className="my-5">
+            <Empty text="دسته بندی برای نمایش وجود ندارد!" />
+          </div>
+        )}
       </div>
     </section>
   );
--- containers/routes/dashboard/products/list.tsx
+++ containers/routes/dashboard/products/list.tsx
@@ -4,7 +4,6 @@ import { useQuery } from '@tanstack/react-query';
 import Image from 'next/image';
 import toast from 'react-hot-toast';
 import { IoMdTrash } from 'react-icons/io';
-import { IoBagAdd } from 'react-icons/io5';
 import { MdEdit } from 'react-icons/md';
 import { APIdeleteProduct } from '@/actions/routes/dashboard/products/delete-product';
 import { APIgetProducts } from '@/actions/routes/global/get-products';
@@ -39,10 +38,6 @@ export function List() {
     return <Loader />;
   }
 
-  if (fetchProducts.data?.length === 0) {
-    return <Empty text="محصولی برای نمایش وجود ندارد!" />;
-  }
-
   const handleDeleteProduct = async (id: string) => {
     const res = await APIdeleteProduct({
       path: {
@@ -58,100 +53,112 @@ export function List() {
   };
 
   return (
-    <section className="size-full overflow-hidden">
-      <div className="overflow-x-auto rounded-xl border border-gray-200">
-        <table className="w-full min-w-max table-auto text-right text-sm">
-          <thead className="bg-gray-200">
-            <tr>
-              <th className="border-b border-gray-200 p-4">
-                <p className="block text-sm font-medium leading-none antialiased">
-                  #
-                </p>
-              </th>
-              <th className="border-b border-gray-200 p-4">
-                <p className="block text-sm font-medium leading-none antialiased">
-                  تصویر
-                </p>
-              </th>
-              <th className="border-b border-gray-200 p-4">
-                <p className="block text-sm font-medium leading-none antialiased">
-                  عنوان
-                </p>
-              </th>
-              <th className="border-b border-gray-200 p-4">
-                <p className="block text-sm font-medium leading-none antialiased">
-                  توضیحات
-                </p>
-              </th>
-              <th className="border-b border-gray-200 p-4">
-                <p className="block text-sm font-medium leading-none antialiased">
-                  مبلغ با تخفیف
-                </p>
-              </th>
-              <th className="border-b border-gray-200 p-4">
-                <p className="block text-sm font-medium leading-none antialiased">
-                  مبلغ بدون تخفیف
-                </p>
-              </th>
-              <th className="border-b border-gray-200 p-3">
-                <button onClick={handleShowModalAddProduct}>
-                  <IoBagAdd size={22} />
-                </button>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {fetchProducts.data?.map((item, index) => (
-              <tr key={item._id} className="odd:bg-gray-50">
-                <td className="px-4 py-1">{index + 1}</td>
-                <td className="px-4 py-1 text-center">
-                  <Image
-                    src={`${process.env.BASE_URL}${item.image.path}`}
-                    alt={item.title}
-                    width={60}
-                    height={60}
-                  />
-                </td>
-                <td className="max-w-[150px] truncate px-4 py-1 text-right">
-                  {item.title}
-                </td>
-                <td className="max-w-[200px] truncate px-4 py-1 text-right">
-                  {item.description}
-                </td>
-                <td className="px-4 py-1">
-                  {formatPrice(item.priceWithDiscount)}
-                </td>
-                <td className="px-4 py-1">
-                  {formatPrice(item.priceWithoutDiscount)}
-                </td>
-                <td className="px-4 py-1 text-right">
-                  <button
-                    onClick={() =>
-                      handleShowModalEditProduct({
-                        title: item.title,
-                        description: item.description,
-                        priceWithDiscount: item.priceWithDiscount,
-                        priceWithoutDiscount: item.priceWithoutDiscount,
-                        category: item.categoryID,
-                        id: item._id,
-                        image: `${process.env.BASE_URL}${item.image.path}`,
-                      })
-                    }
-                    className="mx-2"
-                  >
-                    <MdEdit size={22} />
-                  </button>
-                  <button
-                    onClick={() => handleDeleteProduct(item._id)}
-                    className="mx-2"
-                  >
-                    <IoMdTrash size={22} />
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+    <section className="flex size-full flex-col gap-3 overflow-hidden">
+      {/* actions */}
+      <button
+        onClick={handleShowModalAddProduct}
+        className="w-fit rounded-lg bg-teal p-2 text-smp text-white"
+      >
+        افزودن محصول
+      </button>
+      {/* table */}
+      <div className="relative flex size-full h-fit flex-col overflow-auto rounded-xl border border-gray-200 bg-white bg-clip-border text-gray-600">
+        {fetchProducts.data?.length !== 0 ? (
+          <div>
+            <table className="w-full min-w-max table-auto text-right text-sm">
+              <thead className="bg-gray-200">
+                <tr>
+                  <th className="border-b border-gray-200 p-4">
+                    <p className="block text-sm font-medium leading-none antialiased">
+                      #
+                    </p>
+                  </th>
+                  <th className="border-b border-gray-200 p-4">
+                    <p className="block text-sm font-medium leading-none antialiased">
+                      تصویر
+                    </p>
+                  </th>
+                  <th className="border-b border-gray-200 p-4">
+                    <p className="block text-sm font-medium leading-none antialiased">
+                      عنوان
+                    </p>
+                  </th>
+                  <th className="border-b border-gray-200 p-4">
+                    <p className="block text-sm font-medium leading-none antialiased">
+                      توضیحات
+                    </p>
+                  </th>
+                  <th className="border-b border-gray-200 p-4">
+                    <p className="block text-sm font-medium leading-none antialiased">
+                      مبلغ با تخفیف
+                    </p>
+                  </th>
+                  <th className="border-b border-gray-200 p-4">
+                    <p className="block text-sm font-medium leading-none antialiased">
+                      مبلغ بدون تخفیف
+                    </p>
+                  </th>
+                  <th />
+                </tr>
+              </thead>
+              <tbody>
+                {fetchProducts.data?.map((item, index) => (
+                  <tr key={item._id} className="odd:bg-gray-50">
+                    <td className="px-4 py-1">{index + 1}</td>
+                    <td className="px-4 py-1 text-center">
+                      <Image
+                        src={`${process.env.BASE_URL}${item.image.path}`}
+                        alt={item.title}
+                        width={60}
+                        height={60}
+                      />
+                    </td>
+                    <td className="max-w-[150px] truncate px-4 py-1 text-right">
+                      {item.title}
+                    </td>
+                    <td className="max-w-[200px] truncate px-4 py-1 text-right">
+                      {item.description}
+                    </td>
+                    <td className="px-4 py-1">
+                      {formatPrice(item.priceWithDiscount)}
+                    </td>
+                    <td className="px-4 py-1">
+                      {formatPrice(item.priceWithoutDiscount)}
+                    </td>
+                    <td className="px-4 py-1 text-right">
+                      <button
+                        onClick={() =>
+                          handleShowModalEditProduct({
+                            title: item.title,
+                            description: item.description,
+                            priceWithDiscount: item.priceWithDiscount,
+                            priceWithoutDiscount: item.priceWithoutDiscount,
+                            category: item.categoryID,
+                            id: item._id,
+                            image: `${process.env.BASE_URL}${item.image.path}`,
+                          })
+                        }
+                        className="mx-2"
+                      >
+                        <MdEdit size={22} />
+                      </button>
+                      <button
+                        onClick={() => handleDeleteProduct(item._id)}
+                        className="mx-2"
+                      >
+                        <IoMdTrash size={22} />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div className="my-5">
+            <Empty text="محصولی برای نمایش وجود ندارد!" />
+          </div>
+        )}
       </div>
     </section>
   );
